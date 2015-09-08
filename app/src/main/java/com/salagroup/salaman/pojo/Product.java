136960f276e160907ee77ab928cd8c2e4306b916
unknown <tryto.ss94@gmail.com>
2015-09-08 12:22:29 +0700

    Test Load Image from Server

---
+++ app/src/main/java/com/salagroup/salaman/pojo/Product.java
@@ -0,0 +1,45 @@
+package com.salagroup.salaman.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+
+public class Product {
+
+    @Expose
+    private Long productId;
+    @Expose
+    private String productName;
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    @Expose
+    private List<String> images = new ArrayList<String>();
+
+    /**
+     * @return The productId
+     */
+    public Long getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId The productId
+     */
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @return The productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+}
