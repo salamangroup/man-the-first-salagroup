73262abc983715d95fedfe67b1fd8aa7f6f6b5e6
bvhloc <bvhloc@gmail.com>
2015-10-11 11:59:15 +0700

    fix CustomerDetail + more in ValidationHelper
    CustomerDetail:
    + Address limit 100 charater
    + Add CustomerGroup button
    ValidationHelper:
    + getValidFormatMoney
    + getViFormatMoney
    + addViMoneyTextChanged

--- app/src/main/java/com/salagroup/salaman/helper/ValidationHelper.java
+++ app/src/main/java/com/salagroup/salaman/helper/ValidationHelper.java
@@ -24,7 +24,7 @@ public class ValidationHelper {
     }
 
     public static boolean checkViHumanNameString(String s) {
-        Pattern pattern = Pattern.compile("[^" + Constant.VIETNAMESE_DIACRITIC_CHARACTERS + Constant.ALPHABET_CHARACTERS + " " + "]", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("[^" + Constant.VIETNAMESE_DIACRITIC_CHARACTERS + Constant.ALPHABET_CHARACTERS + Constant.DIGITS + " ." + "]", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(s);
         return !matcher.find();
     }
@@ -80,9 +80,9 @@ public class ValidationHelper {
         });
     }
 
-    public static String getValidFormatPhone(String phoneNumber) {
+    public static String getValidFormatPhone(String phone) {
 
-        phoneNumber = phoneNumber.replaceAll(" ", "");
+        phone = phone.replaceAll(" ", "");
 //        phoneNumber = phoneNumber.replace("-", "");
 //        phoneNumber = phoneNumber.replace("+", "");
 //        phoneNumber = phoneNumber.replace("*", "");
@@ -94,10 +94,10 @@ public class ValidationHelper {
 //        phoneNumber = phoneNumber.replace(".", "");
 //        phoneNumber = phoneNumber.replace(";", "");
 
-        return phoneNumber;
+        return phone;
     }
 
-    public static String getViFormatPhone(String phone){
+    public static String getViFormatPhone(String phone) {
 
         phone = phone.substring(0, 7) + " " + phone.substring(7);
         phone = phone.substring(0, 4) + " " + phone.substring(4);
@@ -126,22 +126,127 @@ public class ValidationHelper {
             @Override
             public void afterTextChanged(Editable s) {
 
-                int afterLength = edt.getText().toString().length();
+                int afterLength = s.length();
+                if (afterLength == 5) {
 
-                String phone = s.toString();
-                String realPhone = phone.replaceAll(" ", "");
-                if (afterLength < beforeLength && realPhone.length() < 10) {
+                    if (afterLength > beforeLength) {
 
-                    edt.setText(realPhone);
-                    edt.setSelection(realPhone.length());
+                        StringBuilder strBuilder = new StringBuilder(s.toString());
+                        strBuilder.insert(4, " ");
+                        edt.setText(strBuilder);
+                        edt.setSelection(strBuilder.length());
+
+                    } else {
+
+                        StringBuilder strBuilder = new StringBuilder(s.toString());
+                        strBuilder.delete(strBuilder.length() - 1, strBuilder.length());
+                        edt.setText(strBuilder.toString());
+                        edt.setSelection(strBuilder.length());
+                    }
+                } else if (afterLength == 9) {
+
+                    if (afterLength > beforeLength) {
+
+                        StringBuilder strBuilder = new StringBuilder(s.toString());
+                        strBuilder.insert(8, " ");
+                        edt.setText(strBuilder.toString());
+                        edt.setSelection(strBuilder.length());
+//                        s.insert(s.length() - 1, " ");
+                    } else {
+
+                        StringBuilder strBuilder = new StringBuilder(s.toString());
+                        strBuilder.delete(strBuilder.length() - 1, strBuilder.length());
+                        edt.setText(strBuilder.toString());
+                        edt.setSelection(strBuilder.length());
+                    }
 
                 }
-                if (phone.length() == 10) {
 
-                    phone = getViFormatPhone(phone);
-                    edt.setText(phone);
-                    edt.setSelection(phone.length());
+            }
+        });
+    }
+
+    private static String getValidFormatMoney(String money) {
+
+        money = money.replaceAll(",", "");
+
+        return money;
+    }
+
+    private static String getViFormatMoney(String money) {
+
+//        StringBuilder strBuilder = new StringBuilder(money);
+//        int add = 0;
+//        for (int i = 1; i <= money.length(); i++) {
+//
+//            if (i != 1 && i % 3 == 1) {
+//
+//                strBuilder.insert(money.length() - i - add, ",");
+//                add++;
+//            }
+//        }
+
+        StringBuilder strBuilder = new StringBuilder(money);
+        if(money.length()>12){
+
+            strBuilder.insert(strBuilder.length()-3, ",");
+            strBuilder.insert(strBuilder.length()-7, ",");
+            strBuilder.insert(strBuilder.length()-11, ",");
+            strBuilder.insert(strBuilder.length()-15, ",");
+            return strBuilder.toString();
+        }
+        if(money.length()>9){
+
+            strBuilder.insert(strBuilder.length()-3, ",");
+            strBuilder.insert(strBuilder.length()-7, ",");
+            strBuilder.insert(strBuilder.length()-11, ",");
+            return strBuilder.toString();
+        }
+        if(money.length()>6){
+
+            strBuilder.insert(strBuilder.length()-3, ",");
+            strBuilder.insert(strBuilder.length()-7, ",");
+            return strBuilder.toString();
+        }
+        if(money.length()>=3){
+
+            strBuilder.insert(strBuilder.length()-3, ",");
+            return strBuilder.toString();
+        }
+
+        return strBuilder.toString();
+    }
+
+    public static void addViMoneyTextChanged(final EditText edt) {
+
+
+        edt.addTextChangedListener(new TextWatcher() {
+
+            int beforeLength;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                beforeLength = edt.getText().toString().length();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                int afterLength = s.length();
+                if (afterLength != beforeLength) {
+
+                    String validMoney = getValidFormatMoney(s.toString());
+                    String viMoney = getViFormatMoney(validMoney);
+                    edt.setText(viMoney);
+                    edt.setSelection(viMoney.length());
                 }
+
             }
         });
     }
